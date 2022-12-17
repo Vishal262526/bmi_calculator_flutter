a59996d57eefc2f346bd221392763f54f883284e
Vishal262526 <vishal.kumar190601@gmail.com>
2022-12-17 23:19:13 +0530

    UI Almost Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,232 @@
 import 'package:flutter/material.dart';
 
-void amin() => runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xFF453C67),
+        appBar: AppBar(
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Color(0xFF453C67),
+          title: Text('BMI Calculator'),
+        ),
+        body: Home(),
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  double currentValue = 10;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.male,
+                            color: Colors.white,
+                            size: 60,
+                          ),
+                          Text(
+                            "Male",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      color: Color(0xFF6D67E4),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(16.0),
+                      color: Color(0xFF6D67E4),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.female,
+                            color: Colors.white,
+                            size: 60,
+                          ),
+                          Text(
+                            "Female",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+                margin: EdgeInsets.all(16.0),
+                color: Color(0xFF6D67E4),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Height (cm)',
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Slider(
+                      activeColor: Colors.white,
+                      inactiveColor: Colors.white,
+                      value: currentValue,
+                      min: 0,
+                      max: 100,
+                      onChanged: (double value) {
+                        setState(() {
+                          currentValue = value;
+                        });
+                      },
+                    ),
+                  ],
+                )),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Weight",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            '60.0',
+                            style: TextStyle(fontSize: 40, color: Colors.white),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                color: Colors.white,
+                                padding: EdgeInsets.all(5.0),
+                                child: Icon(Icons.add),
+                              ),
+                              SizedBox(width: 15,),
+                              Container(
+                                color: Colors.white,
+                                padding: EdgeInsets.all(5.0),
+                                child: Icon(Icons.exposure_minus_1),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      color: Color(0xFF6D67E4),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(16.0),
+                      color: Color(0xFF6D67E4),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Age",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            '60.0',
+                            style: TextStyle(fontSize: 40, color: Colors.white),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                color: Colors.white,
+                                padding: EdgeInsets.all(5.0),
+                                child: Icon(Icons.add),
+                              ),
+                              SizedBox(width: 15,),
+                              Container(
+                                color: Colors.white,
+                                padding: EdgeInsets.all(5.0),
+                                child: Icon(Icons.exposure_minus_1),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
